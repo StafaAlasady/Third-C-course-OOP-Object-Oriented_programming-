8bae9f516ac9ff9b23d3ea08877e0f68e58feecb
StafaAlasady <Mustafa.alasady@outlook.com>
2026-09-11 21:00:30 +0200

    Extra bro code stuff to solidify the basics

--- BroCode2.cpp
+++ BroCode2.cpp
@@ -4,15 +4,42 @@
 
 
 
-int main() {
-    std::string name;
+int main(){
 
-    std::cout << " whats your name ?: ";
-    std::cin >> name;
+    // int (number without decimal)
+    int age = 21;
+    int year = 2026;
+    int days = 7.5;
 
-    std::cout << "Hello Sir " << name << '\n';
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 30.5;
 
-    std::cout <<
-    
-    return 0; 
+    char grade = 'A';
+    char initial = 'C';
+
+    char currency = '$';
+
+    std::cout << initial <<'\n';
+    std::cout << currency;
+
+    // boolean (there are only 2 states, either true or false)
+    bool student = false;
+    bool power = true;
+    bool forsale = true;
+
+    //string ( object where you can store more characters)
+    std::string name = "Stafa ";
+    std::string day = "Friday";
+    std::string food = "Burgers";
+    std::string adress = "My own empire";
+
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "where do you live ? " << adress << '\n';
+    std::cout << "My favorite day of the week is " << day << '\n';
+
+
+    return 0;
 }
