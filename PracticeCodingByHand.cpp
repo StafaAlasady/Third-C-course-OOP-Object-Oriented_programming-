5e423e23addbe2a99d69d4007ee2104f5ca6fd02
StafaAlasady <Mustafa.alasady@outlook.com>
2026-09-22 00:39:24 +0200

    a gallon and liters converter based on todaysd prices of fuel in the countries using gallons and sweden. this program is for you that understands prices abroad and is on vacation and vice versa

--- PracticeCodingByHand.cpp
+++ PracticeCodingByHand.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <algorithm>
+#include <cctype>
 
-int x;
-int y;
-
+    double pricePerLiter = 16.6;
+    double pricePerGallon = 6.1;
 
 int main (){
 
-    x = 5;
-    y = 6;
 
-    if (x < y){
-    std::cout << " display y: " << y <<std::endl;
+    double gallons;
+    double liters;
+    std::string choice;
+
+    std::cout << " Do you want to convert liters or gallons? " << std::endl;
+    std::cin >> choice;
+
+    std::transform(choice.begin(), choice.end(),choice.begin(), ::tolower);
+
+    if(choice == "gallons"){
+        std::cout << "youve picked gallons! \n";
+        std::cout << "please input in gallons what you want to convert over to liters \n";
+        std::cin >> gallons;
+        std::cout << gallons * 3.785 << std::endl;
+        std::cout << " Price for gasoline here would be:$ " << gallons * pricePerGallon << std::endl;
+    }else if (choice == "liters")
+    {
+        std::cout << "youve picked liters! \n";
+        std::cout << "please input in liters what you want to convert over to gallons \n";
+        std::cin >> liters;
+        std::cout << liters/3.785 << std::endl;
+
+        std::cout << " Price for gasoline here would be:kr " << liters * pricePerLiter << std::endl;
     }else{
-    std::cout << " display x: " << x <<std::endl;
-    std::cout << " display y: " << y <<std::endl;
+        std::cout << "sorry buddy, Invalid choice";
     }
 
     return 0;
